0a72163d4564e4cdf7062f978e7ae9be211ee679
Boopathi_DataNostradamus <51164330+rajboopathiking@users.noreply.github.com>
2024-02-15 17:48:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import cv2
 import numpy as np
 import os
 from PIL import Image
@@ -7,7 +6,7 @@ import joblib
 import time
 
 # Streamlit app title and description
-st.title("Human Detection ")
+st.title("Human Detection")
 st.write("Upload an image and see the prediction")
 
 model = joblib.load("/Users/godfather_101/Downloads/human detection dataset/Model")
@@ -17,29 +16,26 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg
 
 # Check if an image is uploaded
 if uploaded_file is not None:
-    # Save the uploaded file temporarily
-    temp_image_path = "temp_image.jpg"
-    with open(temp_image_path, "wb") as f:
-        f.write(uploaded_file.read())
+    # Open and display the uploaded image using PIL
+    img = Image.open(uploaded_file)
+    img = img.resize((128, 128))  # Resize the image
 
-    # Read the uploaded image using OpenCV
-    img = cv2.imread(temp_image_path)
-    img = cv2.resize(img,(128,128))
-    img = np.expand_dims(img,axis=0)
+    # Display the image
+    st.image(img, caption='Uploaded Image', use_column_width=True)
 
-    
+    # Convert the image to a numpy array
+    img_array = np.array(img)
+
     if st.button("Predict"):
-        prediction = model.predict(img)
+        # Reshape the image array to match the input shape of the model
+        img_array = img_array.reshape((1, 128, 128, 3))
+
+        # Predict using the loaded model
+        prediction = model.predict(img_array)
+
         with st.spinner():
-            time.sleep(1)
-            if prediction > .5:
-                st.success("Human Detection")
+            time.sleep(1)  # Simulate a delay for demonstration purposes
+            if prediction > 0.5:
+                st.success("Human Detected")
             else:
-                st.success("Human Not Detection")
-
-    # Remove the temporary image file
-    os.remove(temp_image_path)
-
-
-
-
+                st.error("Human Not Detected")
